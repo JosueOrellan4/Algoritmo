2801b84a406980273a1aacdb9edd67f23836ed1f
Josue <168094981+JosueOrellan4@users.noreply.github.com>
2024-05-23 11:58:26 -0300

    Aula 1 e 2 aprendendo a programa java script

---
+++ aula 1.js
@@ -0,0 +1,35 @@
+// aqui eu tenho um comentario
+console.log("Meu nome é Julia")  
+
+var nome = "Josue"
+console.log("Imprimir nome: ")
+console.log(nome)
+
+// texto -> String
+// numeros inteiros -> Int
+// numeros decimais -> Float / Double
+// logicos -> Boolean (False : True)
+
+
+var telefone = "11 99999-9999"
+var valor_Monetario = 54.87
+var valor_Inteiro = 20
+var Verdadeiro_ou_Falso = true
+
+//Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+var rua = "Inezita Barroso" //string
+var bairro = "Jaguari" //string
+var cep = "13473-702"
+var estado = "São Paulo" //string
+
+//Guarde os seus dados (nome, email, telefone, data nascimento, genero) em VARIAVEIS
+var nome = "Josue" //string
+var email = "josue.omontenegro@senacsp.edu.br" //string
+var telefone = "+55 19 98980-5478" //string
+var data_de_nascimento = "19/01/2006" //string
+var genero = "Masculino" //string
+
+//Crie uma VARIAVEL que diz se voce é maior de idadei
+var maior_de_idade = true
+// Crie uma VARIAVEL que diz se voce pode dirigir
+var posso_dirigir = false
---
+++ aula 2.js
@@ -0,0 +1,60 @@
+console.log (1+1)
+console.log (1-1)
+console.log (2*5) 
+console.log (10/2)
+
+var numero1 = 10
+var numero2 = 20
+
+
+var soma = numero1 + numero2
+var subtracao = numero1 - numero2
+var multiplicacao = numero1 * numero2
+var divisao = numero1 / numero2
+
+console.log ("Soma dos valores:", soma)
+console.log ("subtração dos valores:", multiplicacao)
+console.log ("multiplicação dos valores", multiplicacao)
+console.log ("Divisão dos valores:", divisao)
+
+//fazer tabuada do 9 
+var numeromain = 9 
+
+console.log (numeromain , "x 1 =" , 1 * numeromain)
+console.log (numeromain , "x 2 =" , 2 * numeromain)
+console.log (numeromain , "x 3 =" , 3 * numeromain)
+console.log (numeromain , "x 4 =" , 4 * numeromain)
+console.log (numeromain , "x 5 =" , 5 * numeromain)
+console.log (numeromain , "x 6 =" , 6 * numeromain)
+console.log (numeromain , "x 7 =" , 7 * numeromain)
+console.log (numeromain , "x 8 =" , 8 * numeromain)
+console.log (numeromain , "x 9 =" , 9 * numeromain , "\n") // "\n" para pula linha
+console.log (numeromain , "x 10 =", 10* numeromain)
+
+
+/**
+* Atribuição ->    =
+
+* Adição ->        +
+
+* Subtração ->     -
+
+* Divisão ->       /
+
+* Multiplicação -> *
+
+* Negação ->         ! -> !false ->
+--------------------------------
+
+* igual ->         == -> 3 == 3  true
+
+* Diferente ->     ! -> 3 != 3.1 true
+--------------------------------
+* Maior ->         > -> 4 > 3 true
+
+* Menor ->         < -> 4 < 3 false
+--------------------------------
+* Maior igual ->   4 >= 4 true
+
+* Menor igaul ->   5 <= 5 true
+/
